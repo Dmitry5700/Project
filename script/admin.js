b3698854c6af58ccbcf0b51de6128363d898321d
Dmitry <dmitryz5700@gmail.com>
2014-12-16 11:32:54 -0600

    first

---
+++ script/admin.js
@@ -0,0 +1,35 @@
+﻿
+function btnAddClick(e)
+{
+    
+    var lessonTitle = $('.inp-add-lesson');
+    
+
+    if (lessonTitle.val().trim() != '') {
+
+        $('#divContentLesson').append('<div class=\"div-title-lesson\">' + lessonTitle.val() + '</div>');
+        $('.inp-add-lesson').val('').css('display', 'none').fadeIn();
+
+    }
+    else {
+        lessonTitle.css('background-color', 'aliceblue').focus();
+        setTimeout(function () { lessonTitle.css('background-color', 'white') }, 3000);
+    }
+
+}
+
+
+
+
+
+
+function ClearValues()
+{
+
+   $('.inp-add-lesson').val('');
+  
+
+
+}
+
+
